3717408622178b27a211dbf579cf9259432b9d68
Felipe Castro <felipecastrolb@gmail.com>
2024-04-19 16:54:18 -0300

    Player de musica funcional completo

--- script.js
+++ script.js
@@ -5,33 +5,40 @@ const play = document.getElementById('play');
 const cover = document.getElementById('cover');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progess');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
 const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
 
 const theHills = {
     songName : 'The Hills',
     bandName : 'The Weeknd',
-    file : 'the hills'
+    file : 'the hills',
+    liked : false,
 }
 
 const starboy = {
     songName : 'Starboy',
     bandName : 'The Weeknd',
-    file : 'starboy'
+    file : 'starboy',
+    liked : false,
 }
 
 const callOutMyName = {
     songName : 'Call Out My Name',
     bandName : 'The Weeknd',
-    file : 'call out my name'
+    file : 'call out my name',
+    liked : false,
 }
 
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [theHills, starboy, callOutMyName];
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist'))
+?? [theHills, starboy, callOutMyName];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
@@ -57,11 +64,24 @@ function playPauseDecider(){
     }
 }
 
+function likeButtonRender(){
+    if(sortedPlaylist[index].liked === true){
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active');
+    }else{
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.classList.remove('button-active');
+    }
+}
+
 function initializeSong(){
     cover.src = `images/${sortedPlaylist[index].file}.jpg`;
     song.src = `songs/${sortedPlaylist[index].file}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].bandName;
+    likeButtonRender();
 }
 
 function previousSong(){
@@ -84,9 +104,10 @@ function nextSong(){
     playSong();
 }
 
-function updateProgessBar(){
+function updateProgess(){
     const barWidth = (song.currentTime/song.duration)*100;
     currentProgress.style.setProperty('--progress', `${barWidth}%`);
+    songTime.innerText = toHHMMSS(song.currentTime);
 }
 
 function jumpTo(event){
@@ -138,13 +159,40 @@ function nextOrRepeat(){
     }
 }
 
+function toHHMMSS(originalNumber){
+    let hours = Math.floor(originalNumber/3600);
+    let min = Math.floor((originalNumber - hours * 3600)/60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+    
+    return `${hours !== 0 ? hours.toString()
+        .padStart(2, '0') + ":" : ""}${min.toString()
+        .padStart(2, '0')}:${secs.toString()
+        .padStart(2, '0')}`;
+}
+
+function updateTotalTime(){
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked(){
+    if(sortedPlaylist[index].liked === false){
+        sortedPlaylist[index].liked = true;
+    }else{
+        sortedPlaylist[index].liked = false;
+    }
+    likeButtonRender();
+    localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgessBar);
+song.addEventListener('timeupdate', updateProgess);
 song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
