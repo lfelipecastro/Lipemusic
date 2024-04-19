fc3d05e44361d3d59abbf10c03c6505e1b3ba341
Felipe Castro <felipecastrolb@gmail.com>
2024-04-19 14:10:19 -0300

    Quase finalizando o projeto

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const play = document.getElementById('play');
 const cover = document.getElementById('cover');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progess');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
 
 const theHills = {
     songName : 'The Hills',
@@ -25,7 +29,10 @@ const callOutMyName = {
 }
 
 let isPlaying = false;
-const playlist = [theHills, starboy, callOutMyName];
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = [theHills, starboy, callOutMyName];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function playSong(){
@@ -51,30 +58,84 @@ function playPauseDecider(){
 }
 
 function initializeSong(){
-    cover.src = `images/${playlist[index].file}.jpg`;
-    song.src = `songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].bandName;
+    cover.src = `images/${sortedPlaylist[index].file}.jpg`;
+    song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].bandName;
 }
 
 function previousSong(){
     if(index === 0){
-        index = playlist.length - 1;
+        index = sortedPlaylist.length - 1;
     }else{
-        index -= 1
+        index -= 1;
     }
     initializeSong();
     playSong();
 }
 
 function nextSong(){
-    if(index === playlist.length - 1){
+    if(index === sortedPlaylist.length - 1){
         index = 0;
     }else{
-        index += 1
+        index += 1;
     }
     initializeSong();
     playSong();
+}
+
+function updateProgessBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray){
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()* size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButtonClicked(){
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add('button-active');
+    }else{
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove('button-active');
+    }
+}
+
+function repeatButtonClicked(){
+    if(repeatOn === false){
+        repeatOn = true;
+        repeatButton.classList.add('button-active');
+    }else{
+        repeatOn = false;
+        repeatButton.classList.remove('button-active');
+    }
+}
+
+function nextOrRepeat(){
+    if(repeatOn === false){
+        nextSong();
+    }else{
+        playSong();
+    }
 }
 
 initializeSong();
@@ -82,3 +143,8 @@ initializeSong();
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgessBar);
+song.addEventListener('ended', nextOrRepeat);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
